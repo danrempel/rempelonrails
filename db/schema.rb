6d0eba19afb141db542f67f0e5e9a1b22243e45c
Dan Rempel <dan.rempel0@gmail.com>
2016-02-22 14:40:47 -0700

    Added subtitle migration to posts & added them to relevant views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216025620) do
+ActiveRecord::Schema.define(version: 20160222203837) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160216025620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "slug"
+    t.text     "subtitle"
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
