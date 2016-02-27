3bfcbd906896f0c604a525b1f1de2bf251f2ffe6
Dan Rempel <dan.rempel0@gmail.com>
2016-02-27 12:36:37 -0700

    Add post_path block to sitemap.rb

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'will_paginate', '~> 3.1'
 gem 'mail_form', '~> 1.5', '>= 1.5.1'
 gem 'devise', '~> 3.5', '>= 3.5.6'
 gem 'unicorn', '~> 5.0', '>= 5.0.1'
+gem 'sitemap_generator'
 
 group :development, :test do
   gem 'sqlite3'
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,3 @@
-require 'rubygems'
-require 'sitemap_generator'
-
 SitemapGenerator::Sitemap.default_host = 'http://www.rempelonrails.com'
 SitemapGenerator::Sitemap.create do
   add '/', :changefreq => 'weekly', :priority => 0.9
@@ -8,8 +5,8 @@ SitemapGenerator::Sitemap.create do
   add '/posts', :changefreq => 'weekly'
   add '/projects', :changefreq => 'weekly'
 
-  #Post.find_each do |post|
-  #	add post_path(post.slug), lastmod: post.updated_at
-  #end
+  Post.find_each do |post|
+  	add post_path(post.slug), lastmod: post.updated_at, :priority => 0.5
+  end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
 end
