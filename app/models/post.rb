bbeddc28ffe23a81bdce54d68d1489679ecf2725
Dan Rempel <dan.rempel0@gmail.com>
2016-03-01 06:36:43 -0700

    Add url catch for updated slugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   acts_as_taggable_on :tags
 	extend FriendlyId
   friendly_id :title, use: :slugged
+
+  def should_generate_new_friendly_id?
+    new_record?
+  end
 end
