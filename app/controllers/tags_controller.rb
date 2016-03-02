54bf55860c3620760d1660899616149b2f0c51f5
Dan Rempel <dan.rempel0@gmail.com>
2016-03-02 06:52:05 -0700

    Prevent Draft Post from showing on Tag view

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,7 +6,11 @@ class TagsController < ApplicationController
 
   def show
     @tag = ActsAsTaggableOn::Tag.friendly.find(params[:id])
-    @posts = Post.tagged_with(@tag.name).order("created_at desc").paginate(page: params[:page], per_page: 10)
-    prepare_meta_tags(title: @tag.name, description: @tag.name, keywords: @tag_keywords)
+    if user_signed_in?
+      @posts = Post.tagged_with(@tag.name).order("created_at desc").paginate(page: params[:page], per_page: 10)
+    else
+      prepare_meta_tags(title: @tag.name, description: @tag.name, keywords: @tag_keywords)
+      @posts = Post.tagged_with(@tag.name).where(published: true).order("created_at desc").paginate(page: params[:page], per_page: 10)
+    end
   end
 end
