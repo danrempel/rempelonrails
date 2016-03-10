99e53f77a50348da99a8f7ca498e279f95f12fa5
Dan Rempel <dan.rempel0@gmail.com>
2016-03-09 18:11:00 -0700

    Update 404 and 500 pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   devise_for :users
   resources :posts
   resources :tags, only: [:index, :show]
@@ -8,6 +12,8 @@ Rails.application.routes.draw do
   match '/about', to: 'welcome#index', via: 'get'
   get 'welcome/index'
   root 'posts#index'
+  match '/404', :to => 'errors#not_found', :via => :all
+  match '/500', :to => 'errors#internal_server_error', :via => :all
 
   get '*path' => redirect('/')
 end
