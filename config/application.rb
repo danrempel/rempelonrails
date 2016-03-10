f22db958dfb83e02d7c5f87422b924189c6da7f5
Dan Rempel <dan.rempel0@gmail.com>
2016-03-10 14:14:04 -0700

    Blockquote styling

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.exceptions_app = self.routes
-
 module Rempelonrails
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,5 +22,6 @@ module Rempelonrails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.exceptions_app = self.routes
   end
 end
